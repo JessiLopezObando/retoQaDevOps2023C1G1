39c2d175c22b18beeb372af6be3a7526bc55d562
Ivan <idruizbe@gmail.com>
2023-04-13 12:26:34 -0500

    Se hace el primer soap

--- src/main/java/com/sofkau/utils/Path.java
+++ src/main/java/com/sofkau/utils/Path.java
@@ -1,14 +1,14 @@
 package com.sofkau.utils;
 
 public enum Path {
-
-
     SOAP_CAPITAL_BASE_URL("http://webservices.oorsprong.org/"),
     RESOURCE_CAPITAL("websamples.countryinfo/CountryInfoService.wso"),
     BODY_PATH("src/test/resources/soap/filecapital.xml"),
 
     BODY_PATH_PAIS("src/test/resources/soap/filepais.xml"),
-
+    CALCULATOR_BODY_PATH("src/test/resources/soap/filecalculadora.xml"),
+    SOAP_CALCULATOR_URL("http://www.dneonline.com/"),
+    RESOURCE_SUM("calculator.asmx"),
 
     SOAP_PERSONA_BASE_URL("http://www.crcind.com/"),
     RESOURCE_PERSONA("csp/samples/SOAP.Demo.cls"),
