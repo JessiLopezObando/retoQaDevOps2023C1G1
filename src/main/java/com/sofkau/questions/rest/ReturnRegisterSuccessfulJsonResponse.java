9a6475e29c55d8ffe9a550ac1cede37f581a0c58
MiguelMendoza22 <jesusmiguelmm@ufps.edu.co>
2023-04-13 13:43:18 -0500

    se agrega automatización de servicio Post registerSuccessful

---
+++ src/main/java/com/sofkau/questions/rest/ReturnRegisterSuccessfulJsonResponse.java
@@ -0,0 +1,17 @@
+package com.sofkau.questions.rest;
+
+import com.sofkau.models.rest.ResponseRegister;
+import net.serenitybdd.rest.SerenityRest;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+
+public class ReturnRegisterSuccessfulJsonResponse implements Question<ResponseRegister> {
+    @Override
+    public ResponseRegister answeredBy(Actor actor) {
+        return SerenityRest.lastResponse().as(ResponseRegister.class);
+    }
+
+    public static ReturnRegisterSuccessfulJsonResponse returnRegisterSuccessfulJsonResponse(){
+        return new ReturnRegisterSuccessfulJsonResponse();
+    }
+}
