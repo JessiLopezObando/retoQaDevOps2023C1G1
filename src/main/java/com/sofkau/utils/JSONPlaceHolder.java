4142447d8be1b0f921503bf11720d410956e9f69
Ivan <idruizbe@gmail.com>
2023-04-13 13:44:01 -0500

    Se termina la automatizacion

--- src/main/java/com/sofkau/utils/JSONPlaceHolder.java
+++ src/main/java/com/sofkau/utils/JSONPlaceHolder.java
@@ -5,7 +5,8 @@ public enum JSONPlaceHolder {
     PLACE_HOLDER_BASE_URL("https://jsonplaceholder.typicode.com/"),
     JSON_PLACE_HOLDER("https://jsonplaceholder.typicode.com/"),
     GET_RESOURCE("posts/"),
-    GET_POSTS("posts/");
+    GET_POSTS("posts/"),
+    PUT_RESOURCE("comments/");
 
     private final String  value;
 
